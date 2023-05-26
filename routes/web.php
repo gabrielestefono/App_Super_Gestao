bfe68bb1ae5c1d0e994439d969a88d022f963314
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-26 12:09:17 -0300

    Nomeando Rotas

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,14 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-Route::get('/login', function(){return 'Login';});
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobre-nos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
 // Rotas restritas /app
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){return 'Clientes';});
-    Route::get('/fornecedores', function(){return 'Fornecedores';});
-    Route::get('/produtos', function(){return 'Produtos';});
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
 });

d397503cd8df1169eee27b87c3726c36acf8de40
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-27 20:26:02 -0300

    Como incluir comentários no Laravel

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,12 @@
+<h3>Fornecedores</h3>
+
+{{-- Isso é igual à  isso <?= ?>--}}
+
+{{-- Aqui fica o meu comentário, que será descartado pelo interpretador do blade. --}}
+
+@php 
+
+    // Comentários diferentes
+
+@endphp
+

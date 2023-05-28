2609c166b6a7ebac16db8142d551fd52034a3917
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-28 09:47:52 -0300

    @empty

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -9,7 +9,7 @@ class FornecedoresController extends Controller
     public function index(){
         $fornecedores = [
             0=>["nome" => "Fornecedor 1", "status" => "N", 'cnpj' => '00.000.000/000-00'],
-            1=>["nome" => "Fornecedor 2", "status" => "S"],
+            1=>["nome" => "Fornecedor 2", "status" => "S", 'cnpj' => ''],
         ];
         return view("app.fornecedor.index", compact("fornecedores"));
     }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -7,9 +7,13 @@
 @endphp
 
 @isset($fornecedores)
-    <p>Fornecedor: {{$fornecedores[0]['nome']}}</p>
-    <p>Status: {{$fornecedores[0]['status']}}</p>
-    @isset($fornecedores[0]['cnpj'])
-    <p>CPNJ: {{$fornecedores[0]['cnpj']}}</p>
+    <p>Fornecedor: {{$fornecedores[1]['nome']}}</p>
+    <p>Status: {{$fornecedores[1]['status']}}</p>
+    @isset($fornecedores[1]['cnpj'])
+        <p>CPNJ: {{ $fornecedores[1]['cnpj'] }}
+        @empty($fornecedores[1]['cnpj'])
+            Vazio
+        @endempty
+        </p>
     @endisset
-@endisset
+@endisset   

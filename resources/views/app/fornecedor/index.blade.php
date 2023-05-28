2da441fc396ac4720b5a38871917a81678f65e51
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-27 23:18:37 -0300

    @unless/@endunless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,15 +1,8 @@
 <h3>Fornecedores</h3>
 
-@php
+<p>Fornecedor: {{$fornecedores[0]['nome']}}</p>
+<p>Status: {{$fornecedores[0]['status']}}</p>
 
-
-
-@endphp
-
-@if (count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados.</h3>
-@elseif(count($fornecedores) > 10)
-    <h3> Existem vários fornecedores cadastrados <h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados!<h3>
-@endif
+@unless($fornecedores[0]['status'] == 'S')
+<p> Fornecedor Inativo </p>
+@endunless

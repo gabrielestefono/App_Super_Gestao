fb8d3b990f596ae4b96d5d85f6f8f1b8affa5cb1
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-29 19:04:50 -0300

    @forelse

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,11 +1,13 @@
 <h3>Fornecedores</h3>
 
 @isset($fornecedores)
-    @foreach ($fornecedores as $fornecedor)
+    @forelse ($fornecedores as $fornecedor)
         <p>Fornecedor: {{$fornecedor['nome']}}</p>
         <p>Status: {{$fornecedor['status']}}</p>
         <p>CNPJ: {{$fornecedor['cnpj']  ?? "Dado não foi preenchido" }}</p>
         <p>Telefone: {{$fornecedor['ddd']  ?? "Dado não foi preenchido" }} {{$fornecedor['telefone']  ?? "Dado não foi preenchido" }}</p>
         <hr>
-    @endforeach
+        @empty
+            <p> Não existem fornecedores cadastrados!</p>
+    @endforelse
 @endisset

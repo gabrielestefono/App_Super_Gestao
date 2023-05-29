b711d8f30bf60014807ed9e6d52a5dfeac7a5223
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-29 19:40:34 -0300

    Melhorando o visual

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,10 +2,18 @@
 
 @isset($fornecedores)
     @forelse ($fornecedores as $fornecedor)
-        <p>Fornecedor: @{{$fornecedor['nome']}}</p>
+        <p>Iteração Atual: {{ $loop->iteration }}</p>
+        <p>Fornecedor: {{$fornecedor['nome']}}</p>
         <p>Status: {{$fornecedor['status']}}</p>
         <p>CNPJ: {{$fornecedor['cnpj']  ?? "Dado não foi preenchido" }}</p>
         <p>Telefone: {{$fornecedor['ddd']  ?? "Dado não foi preenchido" }} {{$fornecedor['telefone']  ?? "Dado não foi preenchido" }}</p>
+        @if($loop->first)
+            <p> Primeira Iteração do loop </p>
+        @endif
+        @if($loop->last)
+            <p> Última Iteração do loop </p>
+            <p> Total de registros {{$loop->count}} </p>
+        @endif
         <hr>
         @empty
             <p> Não existem fornecedores cadastrados!</p>

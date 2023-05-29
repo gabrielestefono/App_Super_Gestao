c20b8bea358a9da9da83af4a34c1d57f1c4c02f9
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-29 18:54:05 -0300

    @foreach

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,13 +1,11 @@
 <h3>Fornecedores</h3>
 
 @isset($fornecedores)
-    @php $i = 0 @endphp
-    @while(isset($fornecedores[$i]))
-        <p>Fornecedor: {{$fornecedores[$i]['nome']}}</p>
-        <p>Status: {{$fornecedores[$i]['status']}}</p>
-        <p>CNPJ: {{$fornecedores[$i]['cnpj']  ?? "Dado não foi preenchido" }}</p>
-        <p>Telefone: {{$fornecedores[$i]['ddd']  ?? "Dado não foi preenchido" }} {{$fornecedores[$i]['telefone']  ?? "Dado não foi preenchido" }}</p>
+    @foreach ($fornecedores as $fornecedor)
+        <p>Fornecedor: {{$fornecedor['nome']}}</p>
+        <p>Status: {{$fornecedor['status']}}</p>
+        <p>CNPJ: {{$fornecedor['cnpj']  ?? "Dado não foi preenchido" }}</p>
+        <p>Telefone: {{$fornecedor['ddd']  ?? "Dado não foi preenchido" }} {{$fornecedor['telefone']  ?? "Dado não foi preenchido" }}</p>
         <hr>
-        @php $i++ @endphp
-    @endwhile
+    @endforeach
 @endisset

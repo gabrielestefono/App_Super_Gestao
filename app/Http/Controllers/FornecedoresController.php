f2afc0871bd1433b8875951bd55489fb16f5d83d
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-28 23:05:32 -0300

    Condicional Ternário

--- app/Http/Controllers/FornecedoresController.php
+++ app/Http/Controllers/FornecedoresController.php
@@ -9,8 +9,13 @@ class FornecedoresController extends Controller
     public function index(){
         $fornecedores = [
             0=>["nome" => "Fornecedor 1", "status" => "N", 'cnpj' => '00.000.000/000-00'],
-            1=>["nome" => "Fornecedor 2", "status" => "S", 'cnpj' => ''],
+            1=>["nome" => "Fornecedor 2", "status" => "S"],
         ];
+        /*
+        condicao ? valorSim : valorNao;
+        */
+        $msg = isset($fornecedores[1]["cnpj"]) ? "CNPJ Informado" : "CNPJ não informado";
+        echo $msg;
         return view("app.fornecedor.index", compact("fornecedores"));
     }
 }

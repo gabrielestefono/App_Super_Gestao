6cbdf0fc6dbe89daf7b0e996363478dd09737660
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-29 19:09:46 -0300

    Escape de impressão do Blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,7 +2,7 @@
 
 @isset($fornecedores)
     @forelse ($fornecedores as $fornecedor)
-        <p>Fornecedor: {{$fornecedor['nome']}}</p>
+        <p>Fornecedor: @{{$fornecedor['nome']}}</p>
         <p>Status: {{$fornecedor['status']}}</p>
         <p>CNPJ: {{$fornecedor['cnpj']  ?? "Dado não foi preenchido" }}</p>
         <p>Telefone: {{$fornecedor['ddd']  ?? "Dado não foi preenchido" }} {{$fornecedor['telefone']  ?? "Dado não foi preenchido" }}</p>

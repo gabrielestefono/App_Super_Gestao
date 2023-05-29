661094b9515f83f472ae3d18adb379f84f2f7672
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-29 18:45:32 -0300

    @for

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,29 +1,11 @@
 <h3>Fornecedores</h3>
 
-@php
-
-// if(isset($variável)){} // Checa se a variável está definida
-
-@endphp
-
 @isset($fornecedores)
-    <p>Fornecedor: {{$fornecedores[0]['nome']}}</p>
-    <p>Status: {{$fornecedores[0]['status']}}</p>
-    <p>CNPJ: {{$fornecedores[0]['cnpj']  ?? "Dado não foi preenchido" }}</p>
-    <p>Telefone: {{$fornecedores[0]['ddd']  ?? "Dado não foi preenchido" }} {{$fornecedores[0]['telefone']  ?? "Dado não foi preenchido" }}</p>
-    <p>
-    @switch($fornecedores[0]['ddd'])
-        @case('11')
-            São Paulo - SP
-            @break
-        @case('85')
-            Fortaleza - CE
-            @break
-        @case('32')
-            Juiz de fora - MG
-            @break
-        @default
-            Estado não identificado
-    @endswitch
-    </p>
+    @for($i = 0; isset($fornecedores[$i]); $i++)
+        <p>Fornecedor: {{$fornecedores[$i]['nome']}}</p>
+        <p>Status: {{$fornecedores[$i]['status']}}</p>
+        <p>CNPJ: {{$fornecedores[$i]['cnpj']  ?? "Dado não foi preenchido" }}</p>
+        <p>Telefone: {{$fornecedores[$i]['ddd']  ?? "Dado não foi preenchido" }} {{$fornecedores[$i]['telefone']  ?? "Dado não foi preenchido" }}</p>
+        <hr>
+    @endfor
 @endisset

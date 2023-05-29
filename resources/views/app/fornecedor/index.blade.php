d3906f3553257c9e3af180648634ead38d0a9e19
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-29 18:33:39 -0300

    @switch @case @break

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -10,4 +10,20 @@
     <p>Fornecedor: {{$fornecedores[0]['nome']}}</p>
     <p>Status: {{$fornecedores[0]['status']}}</p>
     <p>CNPJ: {{$fornecedores[0]['cnpj']  ?? "Dado não foi preenchido" }}</p>
+    <p>Telefone: {{$fornecedores[0]['ddd']  ?? "Dado não foi preenchido" }} {{$fornecedores[0]['telefone']  ?? "Dado não foi preenchido" }}</p>
+    <p>
+    @switch($fornecedores[0]['ddd'])
+        @case('11')
+            São Paulo - SP
+            @break
+        @case('85')
+            Fortaleza - CE
+            @break
+        @case('32')
+            Juiz de fora - MG
+            @break
+        @default
+            Estado não identificado
+    @endswitch
+    </p>
 @endisset

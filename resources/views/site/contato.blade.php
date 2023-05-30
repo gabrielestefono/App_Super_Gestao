c731faffc82f583b95f8f14f998eb7cd00a6ab24
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-30 00:28:54 -0300

    @component @endcomponent

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -8,25 +8,8 @@
 
     <div class="informacao-pagina">
         <div class="contato-principal">
-            <form action={{ route('site.contato') }} method="post">
-                @csrf
-                <input type="text" placeholder="Nome" class="borda-preta" name="nome">
-                <br>
-                <input type="text" placeholder="Telefone" class="borda-preta" name="telefone">
-                <br>
-                <input type="text" placeholder="E-mail" class="borda-preta" name="email">
-                <br>
-                <select class="borda-preta" name="motivoContato">
-                    <option value="">Qual o motivo do contato?</option>
-                    <option value="1">Dúvida</option>
-                    <option value="2">Elogio</option>
-                    <option value="3">Reclamação</option>
-                </select>
-                <br>
-                <textarea  name="mensagem" class="borda-preta"  name="">Preencha aqui a sua mensagem</textarea>
-                <br>
-                <button type="submit" class="borda-preta">ENVIAR</button>
-            </form>
+            @component('site.layouts._components.form_contato')
+            @endcomponent
         </div>
     </div>  
 </div>

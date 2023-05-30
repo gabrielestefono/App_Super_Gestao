e766770d5afd32232f3446e901f489a45e21848c
gabrielestefono <gabrielestefono@hotmail.com>
2023-05-30 00:05:41 -0300

    GET

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -8,21 +8,22 @@
 
     <div class="informacao-pagina">
         <div class="contato-principal">
-            <form>
-                <input type="text" placeholder="Nome" class="borda-preta">
+            <form action={{ route('site.contato') }} method="get">
+                @csrf
+                <input type="text" placeholder="Nome" class="borda-preta" name="nome">
                 <br>
-                <input type="text" placeholder="Telefone" class="borda-preta">
+                <input type="text" placeholder="Telefone" class="borda-preta" name="telefone">
                 <br>
-                <input type="text" placeholder="E-mail" class="borda-preta">
+                <input type="text" placeholder="E-mail" class="borda-preta" name="email">
                 <br>
-                <select class="borda-preta">
+                <select class="borda-preta" name="motivoContato">
                     <option value="">Qual o motivo do contato?</option>
-                    <option value="">Dúvida</option>
-                    <option value="">Elogio</option>
-                    <option value="">Reclamação</option>
+                    <option value="1">Dúvida</option>
+                    <option value="2">Elogio</option>
+                    <option value="3">Reclamação</option>
                 </select>
                 <br>
-                <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
+                <textarea  name="mensagem" class="borda-preta"  name="">Preencha aqui a sua mensagem</textarea>
                 <br>
                 <button type="submit" class="borda-preta">ENVIAR</button>
             </form>

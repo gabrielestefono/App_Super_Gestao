970d514b0109c06b627bd2cb601442b19e7cc318
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-08 22:52:40 -0300

    Create e Fillable

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    protected $table = "forneccedores";
+    protected $table = "fornecedores";
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
+
     use HasFactory;
 }

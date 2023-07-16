bdaa29e3118f5fa8a608182de8f760de55c3fd90
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-15 22:47:40 -0300

    Gravando dados no banco de dados

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
-        var_dump($_POST);
+    public function contato(Request $request){
+        /*$contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivoContato = $request->input('motivoContato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save();*/
+        $contato = new SiteContato();
+        $contato->create($request->all());
         return view('site.contato', ['titulo'=>'Contato']);
     }
 }
--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -12,5 +12,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
+    protected $fillable = ['nome', 'telefone', 'email', 'motivoContato', 'mensagem'];
     use HasFactory;
 }

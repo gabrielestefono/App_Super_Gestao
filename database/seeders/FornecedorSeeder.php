9d4298fac8a3dbeca819f6ce599e2964883f7212
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-25 00:37:41 -0300

    Utilizando Seeders

--- database/seeders/FornecedorSeeder.php
+++ database/seeders/FornecedorSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use \App\Models\Fornecedor;
+use Illuminate\Support\Facades\DB;
 
 class FornecedorSeeder extends Seeder
 {
@@ -14,11 +15,29 @@ class FornecedorSeeder extends Seeder
      */
     public function run()
     {
+        // Instanciando o objeto
         $fornecedor = new Fornecedor();
         $fornecedor->nome = "fornecedor100";
         $fornecedor->site = "fornecedor100.com.br";
         $fornecedor->uf = "ce";
         $fornecedor->email = "fornecedor100@contato.com.br";
         $fornecedor->save();
+
+
+        // Utilizando o método create
+        Fornecedor::create([
+            "nome"=>"Fornecedor 200",
+            "site"=>"fornecedor200.com.br",
+            "uf"=>"RS",
+            "email"=>"contato@fornecedor200.com.br"
+        ]);
+
+        // Insert 
+        DB::table('fornecedores')->insert([
+            "nome"=>"Fornecedor 300",
+            "site"=>"fornecedor300.com.br",
+            "uf"=>"SP",
+            "email"=>"contato@fornecedor300.com.br"
+        ]);
     }
 }

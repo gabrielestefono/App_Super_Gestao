19d82c764e18f256e6c9bfb959f7a1422aca1542
gabrielestefono <gabrielestefono@hotmail.com>
2023-10-09 06:56:47 -0300

    Customizando a apresentação dos erros

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,10 +2,13 @@
 <form action={{ route('site.contato') }} method="post">
     @csrf
     <input type="text" value="{{old('nome')}}" placeholder="Nome" class="{{ $classe }}" name="nome">
+    {{$errors->first('nome') ? $errors->first('nome') : ''}}
     <br>
     <input type="text" value="{{old('telefone')}}" placeholder="Telefone" class="{{ $classe }}" name="telefone">
+    {{$errors->first('telefone') ? $errors->first('telefone') : ''}}
     <br>
     <input type="text" value="{{old('email')}}" placeholder="E-mail" class="{{ $classe }}" name="email">
+    {{$errors->first('email') ? $errors->first('email') : ''}}
     <br>
     <select class="{{ $classe }}" name="motivo_contatos_id">
         <option value="">Qual o motivo do contato?</option>
@@ -13,14 +16,18 @@
             <option value="{{$motivo_contato->id}}" {{old('motivo_contatos_id')== $motivo_contato->id ? 'selected' : ""}}>{{$motivo_contato->motivo_contato}}</option>
         @endforeach
     </select>
+    {{$errors->first('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : ''}}
     <br>
     <textarea  name="mensagem" class="{{ $classe }}">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui  a sua mensagem'}} </textarea>
+    {{$errors->first('mensagem') ? $errors->first('mensagem') : ''}}
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
 
-<div style="position: absolute; top: 0; left: 0; width: 100%; background: #c96868; color:white;">
-    <pre>
-        {{ print_r($errors) }}
-    </pre>
-</div>
+@if($errors->any())
+    <div style="position: absolute; top: 0; left: 0; width: 100%; background: #c96868; color:white;">
+        @foreach ($errors->all() as $erro)
+            {{$erro}} <br>
+        @endforeach
+    </div>
+@endif

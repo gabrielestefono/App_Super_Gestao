f078b368c96267a0c153de61b18bb34567def2b3
gabrielestefono <gabrielestefono@hotmail.com>
2023-10-09 07:09:12 -0300

    Customizando apresentação de erros

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -29,15 +29,24 @@ class ContatoController extends Controller
 
     public function salvar(Request $request){
         // Validar o recebimento dos dados recebidos no request
-            $request->validate([
+            $regras = [
                 "nome"=>"required|min:3|max:40", /*|unique:site_contatos*/
                 "telefone"=>"required",
                 "email" => "email",
                 "motivo_contatos_id" => "required",
                 "mensagem" => "required|max:2000"
-            ]);
+            ];
 
+            $feedback = [
+                "nome.min" => "O campo 'nome' precisa ter um tamanho mínimo de 3 caracteres!",
+                "nome.max" => "O campo 'nome' deve ter no máximo 40 caracteres!",
+                "nome.unique" => "O nome deve ser único!",
+                "email.email" => "O email informado não é válido!",
+                "required" => "O campo ':attribute' deve ser preenchido!",
+                "mensagem.max" => "A mensagem deve ter no máximo 2000 caracteres!"
+            ];
 
+            $request->validate($regras,$feedback);
 
         SiteContato::create($request->all());
         return redirect()->route('site.index');
